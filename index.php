bd71e5df24671a4898d133ffd8e9a07d81c22d54
Edwin Chen <ec7233@g.rit.edu>
2024-07-02 18:47:15 -0400

    .

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
 		<link href="css/index.css" rel="stylesheet">
 	</head>
 	<!-- Nav Bar Module -->
+	<div id="flexbox">
 	<header><?php include "resources/header.php"?></header>
 	<body>
 		<!-- Content Start -->
 		<div class="body">
+		
 		</div>
 		<!-- Content End -->
 		<!-- Footer Module -->
 		<?php include "resources/footer.php"?>
     </body>
+	</div>
 </html>
---
+++ order.php
@@ -0,0 +1,21 @@
+<!-- HTML Head Module -->
+<?php include "resources/head.php";?>
+        <!-- Title -->
+		<title>Dragon Bay Bakery</title>
+		<!-- It's own stylesheet -->
+		<link href="css/index.css" rel="stylesheet">
+	</head>
+	<!-- Nav Bar Module -->
+	<div id="flexbox">
+	<header><?php include "resources/header.php"?></header>
+	<body>
+		<!-- Content Start -->
+		<div class="body">
+		
+		</div>
+		<!-- Content End -->
+		<!-- Footer Module -->
+		<?php include "resources/footer.php"?>
+    </body>
+	</div>
+</html>
